4d45167cfb9849cbd2dad08939f18db304afd52a
Matt <Geczy@users.noreply.github.com>
2022-11-07 14:05:34 -0600

    rm blog

--- lib/mdx/sources.ts
+++ lib/mdx/sources.ts
@@ -1,18 +1,6 @@
 import * as z from 'zod'
 import { createSource } from '.'
 
-export const Blog = createSource({
-  contentPath: 'content/blog',
-  basePath: '/blog',
-  sortBy: 'date',
-  sortOrder: 'desc',
-  frontMatter: z.object({
-    title: z.string(),
-    date: z.string(),
-    excerpt: z.string().optional(),
-  }),
-})
-
 export const Page = createSource({
   contentPath: 'content/pages',
   basePath: '/',
