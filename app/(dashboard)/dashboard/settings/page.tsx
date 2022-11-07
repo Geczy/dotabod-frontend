e47dfbfe3351b4fae0d013105ee6d253d24656e9
shadcn <arshad@chummun.com>
2022-11-07 16:37:47 +0400

    feat: upgrade to latest next-auth

--- app/(dashboard)/dashboard/settings/page.tsx
+++ app/(dashboard)/dashboard/settings/page.tsx
@@ -1,27 +1,17 @@
-import { headers } from "next/headers"
-import { notFound } from "next/navigation"
+import { redirect } from "next/navigation"
 
-import { getSession } from "@/lib/session"
+import { getCurrentUser } from "@/lib/session"
+import { authOptions } from "@/lib/auth"
 import { DashboardHeader } from "@/components/dashboard-header"
 import { DashboardShell } from "@/components/dashboard-shell"
 import { UserNameForm } from "@/components/user-name-form"
 import { useSession } from "next-auth/react";
 
-async function getUser() {
-  const session = await getSession(headers().get("cookie"));
-
-  if (!session?.user) {
-    return null;
-  }
-
-  return session.user;
-}
-
 export default async function SettingsPage() {
-  const { data: session, status } = useSession();
+  const user = await getCurrentUser()
 
-  if (!session.user) {
-    notFound();
+  if (!user) {
+    return redirect(authOptions.pages.signIn)
   }
 
   return (
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,30 +1,5 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import TwitchProvider from "next-auth/providers/twitch";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { PrismaClient } from "@/lib/prisma";
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/auth";
 
-const prisma = new PrismaClient();
-
-export const authOptions: NextAuthOptions = {
-  // huh any! I know.
-  // This is a temporary fix for prisma client.
-  // @see https://github.com/prisma/prisma/issues/16117
-  adapter: PrismaAdapter(prisma as any),
-  pages: {
-    signIn: "/login",
-  },
-  providers: [
-    TwitchProvider({
-      clientId: process.env.TWITCH_CLIENT_ID,
-      clientSecret: process.env.TWITCH_CLIENT_SECRET,
-    }),
-  ],
-  callbacks: {
-    async session({ session, token, user }) {
-      session.user.id = user.id;
-      return session;
-    },
-  },
-};
-
+// @see ./lib/auth
 export default NextAuth(authOptions);
